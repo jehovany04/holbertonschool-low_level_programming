821150abfc1063a1b70d50ff37c75822facc4522
jehovany04 <7528@holbertonstudents.com>
2023-10-16 07:06:50 -0700

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,9 +9,9 @@ int main(void)
 
 			srand(time(0));
 				n = rand() - RAND_MAX / 2;
-				/* if ( n < 0 = is positive )
-				 if (n > 0 = is negative )
-				 if (n = 0 is zero)
+				/* if  n < 0 = is positive 
+				 if n > 0 = is negative 
+				 if n = 0 is zero */
 
 				return (0);
 }
