6d0f138f2e1ab5b709a2279301b135df6597368a
jehovany04 <7528@holbertonstudents.com>
2023-10-18 15:34:42 -0700

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Print if is positive or negative
+*Return: 0 Always (succès)
+*/
+int main(void)
+
+{
+	char A;
+
+for (A = 'a'; A <= 'z'; A++, "\nq, e")
+putchar (A),
+putchar ('\n');
+
+return (0);
+
+}
