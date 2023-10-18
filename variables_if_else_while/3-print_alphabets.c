131bc91bea8f425dc9ce2cf1572e251ff33942ae
jehovany04 <7528@holbertonstudents.com>
2023-10-18 14:11:13 -0700

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Print if is positive or negative
+*Return: 0 Always (succès)
+*/
+int main(void)
+
+{
+	char L, l;
+
+	for (L = 'a'; L <= 'z'; L++)
+	for (l = 'a'; l <= 'z'; l++)
+
+putchar (L);
+putchar ('\n');
+putchar (l);
+putchar ('\n');
+
+return (0);
+}
