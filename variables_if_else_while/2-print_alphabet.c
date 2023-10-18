12f9142e6e6e98c4262d04d2782de493ca0f6d00
jehovany04 <7528@holbertonstudents.com>
2023-10-18 07:38:11 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Print if is positive or negative
+*Return: 0 Always (succès)
+*/
+
+int main(void)
+{
+		int L;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+	for (L = 'a'; L <= 'z'; L++)
+	putchar("L\n", L)
+
+	return (0);
+}
