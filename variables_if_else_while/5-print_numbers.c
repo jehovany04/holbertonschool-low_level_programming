0abd1efde41f5cebefd92ea50ef4e3efbce95a33
jehovany04 <7528@holbertonstudents.com>
2023-10-19 08:45:40 -0700

    5-print_numbers.c

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,20 @@
-#include <stdlib.h>                                                                                                                             
-#include <time.h>                                                                                                                               
-#include <stdio.h>                                                                                                                              
-/**                                                                                                                                             
- * *main - Print if is positive or negative                                                                                                        
- * *Return: 0 Always (succès)                                                                                                                      
- * */                                                                                                                                              
-int main(void)                                                                                                                                  
-	                                                                                                                                                
-{                                                                                                                                               
-	        char A;                                                                                                                                 
-		                                                                                                                                                
-		for (A = 'a'; A <= 'z'; A++, "\n", "q, e")                                                                                                      
-			putchar (A);                                                                                                                                    
-		putchar ('\n');                                                                                                                                 
-		                                                                                                                                                
-		return (0);                                                                                                                     }
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Print if is positive or negative
+*Return: 0 Always (succès)
+*/
+int main(void)
+
+{
+
+char N;
+
+for (N = '0'; N <= '9' ; N++)
+
+putchar (N);
+putchar ('\n');
+
+return (0);
+}
