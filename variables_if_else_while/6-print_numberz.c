685284f07799f13fdaf2fa351af9fb4fa4330b8f
jehovany04 <7528@holbertonstudents.com>
2023-10-19 09:08:41 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Print if is positive or negative
+*Return: 0 Always (succès)
+*/
+int main(void)
+{
+
+	int i;
+
+
+for (i = 0; i <= 9; i++)
+
+putchar(i + '0');
+putchar('\n');
+
+return (0);
+
+}
