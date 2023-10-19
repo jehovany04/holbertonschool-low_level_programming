9dfe26eeca2721d9e4097bce4395df32a16efbad
jehovany04 <7528@holbertonstudents.com>
2023-10-19 16:10:37 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main
+*Return: 0 Always (succès)
+*/
+
+int main(void)
+{
+	int CH;
+
+for (CH = 0; CH <= 10 ; CH++)
+
+putchar(CH);
+putchar('\n');
+
+return (0);
+
+}
