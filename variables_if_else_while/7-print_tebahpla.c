016e2f80539508948045bd18f5ebef35181c6576
jehovany04 <7528@holbertonstudents.com>
2023-10-19 15:01:32 -0700

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main
+*Return: 0 Always (succès)
+*/
+
+int main(void)
+{
+	int AL;
+
+for (AL = 'z' ; AL >= 'a' ; AL++)
+
+putchar(AL);
+putchar('\n');
+
+return (0);
+
+}
