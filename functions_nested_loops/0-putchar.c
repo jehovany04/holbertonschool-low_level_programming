e7c53def579fd52a3665802dcba7798060f02915
jehovany04 <7528@holbertonstudents.com>
2023-10-20 08:42:08 -0700

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main -print_putchar.c
+*
+*Return: 0 Always (succès)
+*/
+
+int main(void)
+{
+
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+	putchar ('\n');
+
+return (0);
+
+}
