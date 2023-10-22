8a385996c476fdedde9a1a9e6b1b5b44a05cf406
jehovany04 <7528@holbertonstudents.com>
2023-10-22 14:03:46 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+*main -print_putchar.c
+*
+*Return: 0 Always (succès)
+*/
+
+void (print_alphabet)(void)
+{
+char (l);
+
+for (l = 'a'; l <= 'z'; l++)
+
+putchar(l);
+
+
+putchar;
+
+putchar(l);
+
+
+
+
+putchar('\n');
+
+
+}
+
+return (0);
