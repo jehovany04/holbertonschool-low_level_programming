b40ada964c2afe7071359aaa70c196ebc82f5a44
jehovany04 <7528@holbertonstudents.com>
2023-10-22 15:44:39 -0700

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,7 +4,7 @@
 * _isupper - checks if a character is uppercase
 * @c: The character to be checked
 *
-* Return: 1 if c is uppercase, 0 otherwise
+*Return: 1 if c is uppercase, 0 otherwise
 */
 int _isupper(int c)
 {
