1e29a38a3d72fb0a0f7895a399fbc6c4ff990746
jehovany04 <7528@holbertonstudents.com>
2023-10-22 17:54:59 -0700

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,18 +13,14 @@ for (num = 0; num <= 14; num++)
 {
 if (num > 9)
 {
-_putchar('1');
-_putchar(num % 
-_putchar(num %10 + '0');
+_putchar((num / 10) + '0');
+_putchar((num % 10) + '0');
 }
 else
-																		{
-																		_putchar(num + 
-																		{
-																		_putchar(num + ''0');
-																		}
-																		}
-																		_putchar(
+{
+_putcha(num + '0');
 }
-'\n');		    																}
+}
+_putchar('\n');
+}
 }
