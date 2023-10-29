f178a009f63c72863c21733498872282a59b2710
jehovany04 <7528@holbertonstudents.com>
2023-10-29 15:23:31 -0700

    6-puts2

---
+++ pointers_arrays_strings/6-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/* Function prototype */
+void puts2(char *str);
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char *str;
+
+str = "0123456789";
+/* Correct function call for puts2 */
+puts2(str);
+return (0);
+}
---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* puts2 - prints every other character, start with the first character
+* @str: the input string
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+	if (i % 2 == 0)
+	{
+putchar(str[i]);
+}
+i++;
+}
+
+putchar('\n');
+}
