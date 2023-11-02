54bdfaf89efc22e8b89a09bca735e78fe7e13b5a
jehovany04 <7528@holbertonstudents.com>
2023-11-02 06:25:52 -0700

    0-puts_recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* _puts_recursion - Imprime chaîne et new ligne en utilisant la récursivité
-* @s: La chaîne d'entrée à imprimer
-*/
+ * _puts_recursion - Imprime chaîne et new ligne en utilisant la récursivité
+ * @s: La chaîne d'entrée à imprimer
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-	putchar('\n'); /**caractère de fin de chaîne=imprimer nouvelle ligne**/
+	{
+		putchar('\n'); /**caractère de fin de chaîne=imprimer nouvelle ligne**/
 	return;
-
+	}
 	putchar(*s); /** Imprimer le caractère actuel**/
 	_puts_recursion(s + 1); /** Passer au caractère suivant dans la chaîne**/
 }
