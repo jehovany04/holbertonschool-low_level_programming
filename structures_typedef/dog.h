0ac2f3352a9c3137f8947eaab5429da3735bb1df
jehovany04 <7528@holbertonstudents.com>
2023-11-15 09:54:43 -0800

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - Représente un chien avec un nom, un âge et un propriétaire.
+* @name: Pointeur vers la chaîne de caractères du nom
+* @age: Âge du chien
+* @owner: Pointeur vers la chaîne de caractères du propriétaire
+*/
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/** Définir new  type dog_t as a new nom pour le type struct dog **/
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
