c4d860e9c7f05cc4e69b7a3371b58c1ca4ca0297
jehovany04 <7528@holbertonstudents.com>
2023-11-16 06:10:25 -0800

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"                                                                                                                                
+#include <stdio.h> 
+#include <stdlib.h>                                                                                                                             
+#include <string.h>
+
+void (dog_t *d);
+
+}
+if (dog_t == NULL)
+	free (dog_t);
+
+if (*d == NULL)
+	free (*d);
+{
