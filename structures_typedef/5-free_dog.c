cc9fcbaa686c1383718cb50f9611b8cf72632a1b
jehovany04 <7528@holbertonstudents.com>
2023-11-16 06:15:24 -0800

    5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -3,12 +3,24 @@
 #include <stdlib.h>                                                                                                                             
 #include <string.h>
 
-void (dog_t *d);
-
-}
-if (dog_t == NULL)
-	free (dog_t);
-
-if (*d == NULL)
-	free (*d);
+/**
+* free_dog - Libère l'espace mémoire alloué pour une structure dog_t.
+* @d: Pointeur vers la structure dog_t à libérer.
+*/
+void free_dog(dog_t *d)
 {
+	/** Vérifie si le pointeur n'est pas NULL **/
+	if (d != NULL)
+{
+	/** Libère la mémoire allouée pour le nom du chien **/
+		if (d->name != NULL)
+			free(d->name);
+
+	/** Libère la mémoire allouée pour le propriétaire du chien **/
+	if (d->owner != NULL)
+	free(d->owner);
+
+	/** Libère la mémoire allouée pour la structure dog_t **/
+	free(d);
+	}
+}
