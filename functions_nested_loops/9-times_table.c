a4ed658acffed350d87484662c1d92fb015339f1
jehovany04 <7528@holbertonstudents.com>
2023-12-05 15:05:12 -0800

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,16 +14,24 @@ void times_table(void)
 			product = row * column;
 
 			if (column == 0)
+			{
 				_putchar(product + '0');
+			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
 
 				if (product < 10)
+				{
 					_putchar(' ');
+				}
+				else
+				{
+					_putchar((product / 10) + '0');
+				}
 
-				_putchar(product + '0');
+				_putchar((product % 10) + '0');
 			}
 		}
 		_putchar('\n');
