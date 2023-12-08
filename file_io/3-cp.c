2f3d3c9e14302c0875cf4443a258f143eef242af
jehovany04 <7528@holbertonstudents.com>
2023-12-08 07:04:27 -0800

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,16 +8,37 @@
 #define BUFFER_SIZE 1024
 
 /**
- * main - Copies the content of a file to another file
- * @argc: Number of arguments
- * @argv: Array of arguments
- *
- * Return: 0 on success, otherwise the appropriate exit code
+ * copy_file - Copies the content of one file to another
+ * @from: Source file descriptor
+ * @to: Destination file descriptor
  */
+void copy_file(int from, int to)
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t read_bytes, write_bytes;
+
+	do
+	{
+		read_bytes = read(from, buffer, BUFFER_SIZE);
+		if (read_bytes == -1)
+		{
+			perror("Error reading from file");
+			exit(98);
+		}
+
+		write_bytes = write(to, buffer, read_bytes);
+		if (write_bytes == -1 || write_bytes != read_bytes)
+		{
+			perror("Error writing to file");
+			exit(99);
+		}
+
+	} while (read_bytes > 0);
+}
+
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, read_bytes, write_bytes;
-	char buffer[BUFFER_SIZE];
+	int fd_from, fd_to;
 	mode_t file_permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
@@ -29,42 +50,23 @@ int main(int argc, char *argv[])
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		perror("Error opening source file");
 		exit(98);
 	}
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_permissions);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		perror("Error opening destination file");
 		close(fd_from);
 		exit(99);
 	}
 
-	do
-	{
-		read_bytes = read(fd_from, buffer, BUFFER_SIZE);
-		if (read_bytes == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			close(fd_from);
-			close(fd_to);
-			exit(98);
-		}
-
-		write_bytes = write(fd_to, buffer, read_bytes);
-		if (write_bytes == -1 || write_bytes != read_bytes)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
-	} while (read_bytes > 0);
+	copy_file(fd_from, fd_to);
 
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
+		perror("Error closing file descriptor");
 		exit(100);
 	}
 
