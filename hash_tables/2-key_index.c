3e7dfba1094e61d538c2a5b2ffe91496a1a7ea8c
jehovany04 <7528@holbertonstudents.com>
2023-12-08 06:37:49 -0800

    1-djb2.c

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,20 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Returns the index at which the key should be stored in the hash table
- * @key: The key to find the index for
- * @size: The size of the array of the hash table
+ * key_index - function
+ * @key: const unsigned char ptr
+ * @size: unsigned long int
  *
- * Return: The index at which the key should be stored
- */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+ * Return: unsigned long int
+*/
+unsigned long int	key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_value;
-
-    if (key == NULL || size == 0)
-        return 0;
-
-    hash_value = hash_djb2(key);
-    return (hash_value % size);
+	return (hash_djb2(key) % size);
 }
-
